6cd77d82005a77d861e889d43a3980ef096d392a
Lawrence <lawrencegodfrey58@gmail.com>
2020-09-12 09:24:14 +0200

    added loading bar. changed to 3 classes

--- Segmentation/TrainSegmentationSingleClass.py
+++ Segmentation/TrainSegmentationSingleClass.py
@@ -1,42 +1,39 @@
 import cv2
 import numpy as np
-
 import sys
 import argparse
 import os
-import json
-from tqdm import tqdm 
+import json 
 
 import tensorflow as tf
 import segmentation_models as sm
-
 
 parser = argparse.ArgumentParser(description='Train a segmentation model on a video dataset')
 
 parser.add_argument('--input_json_file', type=str, metavar='', default=None, help='The path to the json file containing the dataset file names')
 
-parser.add_argument('-i', '--input_video_folders',      type=str, metavar='', nargs='+', default=None, help='The path to the folder containing the videos to train on')
-parser.add_argument('-m', '--input_mask_folders',       type=str, metavar='', nargs='+', default=None, help='The path to the folder containing the masked videos to train on')
+parser.add_argument('-i', '--input_Video_folders',      type=str, nargs='+', metavar='', default=None, help='The path to the folder containing the videos to train on')
+parser.add_argument('-m', '--input_Mask_folders',       type=str, nargs='+', metavar='', default=None, help='The path to the folder containing the masked videos to train on')
 
-parser.add_argument('-c', '--model_checkpoint_folder', type=str, metavar='',   default = "./checkpoints/",    help='The folder to save model checkpoints')
-parser.add_argument('-b', '--model_type',              type=str, metavar='',   default='vgg19', help='The model backbone. Default=vgg19')
-parser.add_argument('-s', '--show_video', 			   action='store_true', help='Whether or not to show the input and mask video while reading it in')
+parser.add_argument('-c', '--Model_Checkpoint_Folder', type=str, metavar='',   help='The folder to save model checkpoints')
+parser.add_argument('-b', '--Model_type',              type=str, metavar='',   default='vgg19', help='The model backbone. Default=vgg19')
+parser.add_argument('-s', '--show_video', 			   action='store_true', metavar='', help='Whether or not to show the input and mask video while reading it in')
 parser.add_argument('-p', '--percent_val',             type=float, metavar='', default = 0.2,    help='percentage of dataset to use as validation')
 
 args = parser.parse_args()
 
-if args.input_video_folders is not None and args.input_mask_folders is not None:
-	input_folders = args.input_video_folders
-	input_mask_folders = args.input_mask_folders
+if args.input_Video_folder is not None:
+	input_folders = args.input_Video_folder
+	input_mask_folders = args.input_Mask_folder
 	
 	all_input_filenames, all_mask_filenames = [],[]
 
 	for input_folder, input_mask_folder in zip(input_folders, input_mask_folders):
-		for filename in os.listdir(input_folder):
-			all_input_filenames.append(input_folder + filename) 
-		for filename in os.listdir(input_mask_folder):
-			all_mask_filenames.append(input_mask_folder + filename)
+		all_input_filenames.append((input_folder + filename for filename in os.listdir(input_folder)))
+		all_mask_filenames.append((input_mask_folder + filename for filename in os.listdir(input_mask_folder)))
 
+	print(all_input_filenames)
+	print(all_mask_filenames)
 
 elif args.input_json_file is not None:
 	json_file = args.input_json_file
@@ -50,13 +47,14 @@ elif args.input_json_file is not None:
 else:
 	sys.exit("Either --input_json_file or --input_Video_folder required")
 
+sys.exit()
+checkpoint_path = args.Model_Checkpoint_Folder
 
-checkpoint_path = args.model_checkpoint_folder
 
 # Set up model
-num_classes = 3
+num_classes = 1
 
-BACKBONE = args.model_type
+BACKBONE = args.Model_type
 preprocess_input = sm.get_preprocessing(BACKBONE)
 
 model = sm.Unet(BACKBONE, encoder_weights='imagenet', classes=num_classes)
@@ -67,12 +65,13 @@ model.compile(
     metrics=[sm.metrics.iou_score],
 )
 
+# model.summary()
 
-# Read all videos into these arrays
 all_frames, all_masks = [], []
 
 for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 	print('Reading ' + filename)
+
 
 	# read in files 
 	vid = cv2.VideoCapture(filename)
@@ -84,18 +83,8 @@ for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 	if not mask_vid.isOpened():
 		sys.exit('Video File, ' + mask_filename + ', couldn\'t be opened')
 
-	# make sure total number of frames in the videos are equal
-	length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-	mask_length = int(mask_vid.get(cv2.CAP_PROP_FRAME_COUNT))
-
-	assert length == mask_length
-
-	# destination dimensions of videos
 	xSize = 320
 	ySize = 320
-
-	# Progress Bar 
-	pbar = tqdm(total=length)
 
 	while vid.isOpened() and mask_vid.isOpened:
 
@@ -113,13 +102,8 @@ for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 			mask_frame = cv2.cvtColor(mask_frame, cv2.COLOR_BGR2GRAY)
 			
 			# convert single array into 4 arrays. This is necessary for training
-			sky = np.where(frame==0, 1, 0).astype(np.uint8)
-			sea = np.where(frame==255, 1, 0).astype(np.uint8)
-			boat = np.where(frame==198, 1, 0).astype(np.uint8)
-			# other = np.where(np.logical_and(mask_frame>90 , mask_frame<100), 1, 0).astype(np.uint8)
-
-			# stack 4 into one array
-			label = np.dstack((sky, sea, boat))
+			label = np.where(mask_frame>240, 1, 0).astype(np.uint8)
+			label = label.reshape((320, 320, 1))
 
 			if args.show_video:
 				cv2.imshow('Mask', mask_frame)
@@ -127,27 +111,22 @@ for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 			all_frames.append(frame)
 			all_masks.append(label)
 
-			pbar.update(1)
-		
 			if args.show_video:
 				if cv2.waitKey(1) == 27:
 					vid.release()
 					mask_vid.release()
 					cv2.destroyAllWindows()
-					pbar.close()
 					sys.exit('Video closed')
 
 		else:
+			print('Frame not available')
 			break
 
-	pbar.close()
 	vid.release()
 	mask_vid.release()
 	if args.show_video:
 		cv2.destroyAllWindows()
-	
 
-sys.exit()
 
 # calculate number of frames to use for training
 num_train_examples = int(len(all_frames)*(1-args.percent_val))
@@ -169,22 +148,6 @@ x_val,   y_val    =  all_frames[num_train_examples:],  all_masks[num_train_examp
 # x_train, y_train, x_val, y_val = x_train.reshape((-1, xSize, ySize, 3)), y_train.reshape((-1, xSize, ySize, num_classes)), x_val.reshape((-1, xSize, ySize, 3)), y_val.reshape((-1, xSize, ySize, num_classes))
 
 # preprocess input
-all_frames = preprocess_input(all_frames)
-
-# save checkpoints while training
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-												save_weights_only=True,
-												verbose=1)
-
-
-
-# create two datagen instances with the same arguments
-data_gen_args = dict(   rotation_range=20,
-						width_shift_range=0.2,
-						height_shift_range=0.2,
-						horizontal_flip=True, 
-						validation_split=args.percent_val)
-
 x_train = preprocess_input(x_train)
 x_val   = preprocess_input(x_val)
 
@@ -198,11 +161,10 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 
 
 # create two datagen instances with the same arguments
-data_gen_args = dict()  # rotation_range=20,
-						# width_shift_range=0.2,
-						# height_shift_range=0.2,
-						# horizontal_flip=True, 
-						# validation_split=args.percent_val)
+data_gen_args = dict(   rotation_range=20,
+						width_shift_range=0.2,
+						height_shift_range=0.2,
+						horizontal_flip=True)
 
 image_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
 mask_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
@@ -224,15 +186,17 @@ val_Y_generator = mask_datagen.flow(
 	y_val, 
     seed=seed)
 
+
 # combine generators into one which yields image and masks
 train_generator = zip(train_X_generator, train_Y_generator)
 validation_generator = zip(val_X_generator, val_Y_generator)
 
+
 model.fit(
     x=train_generator,
 	validation_data=validation_generator,
-	validation_steps=50,
-    steps_per_epoch=1000,
+	validation_steps=100,
+    steps_per_epoch=5,
     epochs=50,
 	callbacks=[cp_callback] 
 )
