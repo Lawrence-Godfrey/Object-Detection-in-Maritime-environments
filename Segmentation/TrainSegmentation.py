f8766ea785e5e8b4a7de051f585f6a9a92306742
Lawrence <lawrencegodfrey58@gmail.com>
2020-09-20 14:23:38 +0200

    takes test folder cl argument

--- Segmentation/TrainSegmentation.py
+++ Segmentation/TrainSegmentation.py
@@ -10,51 +10,50 @@ from tqdm import tqdm
 import tensorflow as tf
 import segmentation_models as sm
 
+#TODO Allow for test video folder input 
+#TODO Save history as .pickle
+#TODO More augmentation
+#TODO Automatic end when plateau
+#TODO Try with grayscale input
+#TODO Save linux image
 
 parser = argparse.ArgumentParser(description='Train a segmentation model on a video dataset')
 
-parser.add_argument('--input_json_file', type=str, metavar='', default=None, help='The path to the json file containing the dataset file names')
+parser.add_argument('-i', '--input_video_folders',      type=str, metavar='', nargs='+', required=True, help='The path to the folder containing the videos to train on')
+parser.add_argument('-m', '--input_mask_folders',       type=str, metavar='', nargs='+', required=True, help='The path to the folder containing the masked videos to train on')
 
-parser.add_argument('-i', '--input_video_folders',      type=str, metavar='', nargs='+', default=None, help='The path to the folder containing the videos to train on')
-parser.add_argument('-m', '--input_mask_folders',       type=str, metavar='', nargs='+', default=None, help='The path to the folder containing the masked videos to train on')
+parser.add_argument('-t', '--input_test_video_folders', type=str, metavar='', nargs='+', required=True, help='The path to the folder containing the videos to test on')
+parser.add_argument('-y', '--input_test_mask_folders',  type=str, metavar='', nargs='+', required=True, help='The path to the folder containing the masked videos to test on')
 
-parser.add_argument('-c', '--model_checkpoint_folder', type=str, metavar='',   default = "./checkpoints/",    help='The folder to save model checkpoints')
 parser.add_argument('-b', '--model_type',              type=str, metavar='',   default='vgg19', help='The model backbone. Default=vgg19')
+parser.add_argument('-c', '--model_checkpoint_folder', type=str, metavar='',   default = "./checkpoints/",    help='The folder to save model checkpoints')
 parser.add_argument('-s', '--show_video', 			   action='store_true', help='Whether or not to show the input and mask video while reading it in')
-parser.add_argument('-p', '--percent_val',             type=float, metavar='', default = 0.2,    help='percentage of dataset to use as validation')
+
 
 args = parser.parse_args()
 
-if args.input_video_folders is not None and args.input_mask_folders is not None:
-	input_folders = args.input_video_folders
-	input_mask_folders = args.input_mask_folders
-	
-	all_input_filenames, all_mask_filenames = [],[]
+# read in filenames from arguments
+training_folders = args.input_video_folders
+training_mask_folders = args.input_mask_folders
 
-	for input_folder, input_mask_folder in zip(input_folders, input_mask_folders):
-		for filename in os.listdir(input_folder):
-			all_input_filenames.append(input_folder + filename) 
-		for filename in os.listdir(input_mask_folder):
-			all_mask_filenames.append(input_mask_folder + filename)
+test_folders = args.input_test_video_folders
+test_mask_folders = args.input_test_mask_folders
 
+training_files, training_mask_files = [],[]
+test_files, test_mask_files = [],[]
 
-elif args.input_json_file is not None:
-	json_file = args.input_json_file
-	
-	with open(json_file) as file:
-		data = json.load(file)
+for folder, mask_folder in zip(training_folders, training_mask_folders):
+	for filename in os.listdir(folder):
+		training_files.append(folder + filename) 
 
-	all_input_filenames = data['Videos']['SMD']
-	all_mask_filenames = data['Masks']['SMD']
-
-else:
-	sys.exit("Either --input_json_file or --input_Video_folder required")
+	for filename in os.listdir(mask_folder):
+		training_mask_files.append(mask_folder + filename)
 
 
 checkpoint_path = args.model_checkpoint_folder
 
 # Set up model
-num_classes = 3
+num_classes = 1
 
 BACKBONE = args.model_type
 preprocess_input = sm.get_preprocessing(BACKBONE)
@@ -67,40 +66,27 @@ model.compile(
     metrics=[sm.metrics.iou_score],
 )
 
-
-# Read all videos into these arrays
-all_frames, all_masks = [], []
-
-for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
+def read_frames_to_list(filename, x, is_mask=False):
 	print('Reading ' + filename)
 
-	# read in files 
+	# read in file
 	vid = cv2.VideoCapture(filename)
-	mask_vid = cv2.VideoCapture(mask_filename)
 
 	if not vid.isOpened():
 		sys.exit('Video File, ' + filename + ', couldn\'t be opened')
-	
-	if not mask_vid.isOpened():
-		sys.exit('Video File, ' + mask_filename + ', couldn\'t be opened')
 
-	# make sure total number of frames in the videos are equal
-	length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-	mask_length = int(mask_vid.get(cv2.CAP_PROP_FRAME_COUNT))
-
-	assert length == mask_length
 
 	# destination dimensions of videos
 	xSize = 320
 	ySize = 320
 
 	# Progress Bar 
+	length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 	pbar = tqdm(total=length)
 
-	while vid.isOpened() and mask_vid.isOpened:
+	while vid.isOpened():
 
 		available, frame = vid.read()
-		_, mask_frame = mask_vid.read()
 
 		if available:
 
@@ -109,30 +95,26 @@ for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 			if args.show_video:
 				cv2.imshow('Video', frame)
 
-			mask_frame = cv2.resize(mask_frame, (xSize, ySize), interpolation=cv2.INTER_AREA)
-			mask_frame = cv2.cvtColor(mask_frame, cv2.COLOR_BGR2GRAY)
-			
-			# convert single array into 4 arrays. This is necessary for training
-			sky = np.where(frame==0, 1, 0).astype(np.uint8)
-			sea = np.where(frame==255, 1, 0).astype(np.uint8)
-			boat = np.where(frame==198, 1, 0).astype(np.uint8)
-			# other = np.where(np.logical_and(mask_frame>90 , mask_frame<100), 1, 0).astype(np.uint8)
+			if is_mask:
+				frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				
+				# convert single array into 4 arrays. This is necessary for training
+				sky = np.where(frame<=50, 255, 0).astype(np.uint8)
+				sea = np.where(frame>=220, 255, 0).astype(np.uint8)
+				boat = np.where(frame==198, 255, 0).astype(np.uint8)
+				# other = np.where(np.logical_and(mask_frame>90 , mask_frame<100), 1, 0).astype(np.uint8)
 
-			# stack 4 into one array
-			label = np.dstack((sky, sea, boat))
+				# stack 4 into one array
+				frame = np.dstack((sky, sea, boat))
+				cv2.imshow('label', frame)
 
-			if args.show_video:
-				cv2.imshow('Mask', mask_frame)
-
-			all_frames.append(frame)
-			all_masks.append(label)
+			x.append(frame)
 
 			pbar.update(1)
 		
 			if args.show_video:
-				if cv2.waitKey(1) == 27:
+				if cv2.waitKey(30) == 27:
 					vid.release()
-					mask_vid.release()
 					cv2.destroyAllWindows()
 					pbar.close()
 					sys.exit('Video closed')
@@ -142,34 +124,36 @@ for filename, mask_filename in zip(all_input_filenames, all_mask_filenames):
 
 	pbar.close()
 	vid.release()
-	mask_vid.release()
 	if args.show_video:
 		cv2.destroyAllWindows()
-	
+
+
+# Read all training videos into these arrays
+training_frames, training_masks = [], []
+test_frames, test_masks = [], []
+
+for filename, mask_filename in zip(training_files, training_mask_files):
+	read_frames_to_list(filename, training_frames)
+	read_frames_to_list(mask_filename, training_masks, is_mask=True)
+
+	assert len(training_frames) == len(training_masks), filename + " length not equal to " + mask_filename + " length"
+
+for filename, mask_filename in zip(test_files, test_mask_files):
+	read_frames_to_list(filename, test_frames)
+	read_frames_to_list(mask_filename, test_masks, is_mask=True)
+
+	assert len(test_frames) == len(test_masks), filename + " length not equal to " + mask_filename + " length"
 
 sys.exit()
 
-# calculate number of frames to use for training
-num_train_examples = int(len(all_frames)*(1-args.percent_val))
 
 # convert lists to numpy arrays
-all_frames, all_masks = np.array(all_frames), np.array(all_masks)
-
-# Shuffle the arrays
-rng_state = np.random.get_state()
-np.random.shuffle(all_frames)
-np.random.set_state(rng_state)
-np.random.shuffle(all_masks)
-
-# split whole video into training and validation
-x_train, y_train,  = all_frames[0:num_train_examples], all_masks[0:num_train_examples]
-x_val,   y_val    =  all_frames[num_train_examples:],  all_masks[num_train_examples:]
-
-# reshape to make sure it's in the right format for tensorflow
-# x_train, y_train, x_val, y_val = x_train.reshape((-1, xSize, ySize, 3)), y_train.reshape((-1, xSize, ySize, num_classes)), x_val.reshape((-1, xSize, ySize, 3)), y_val.reshape((-1, xSize, ySize, num_classes))
+x_train, y_train = np.array(training_frames), np.array(training_masks)
+x_val, y_val = np.array(test_frames), np.array(test_masks)
 
 # preprocess input
-all_frames = preprocess_input(all_frames)
+x_train = preprocess_input(x_train)
+x_val = preprocess_input(x_val)
 
 # save checkpoints while training
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
@@ -182,27 +166,8 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 data_gen_args = dict(   rotation_range=20,
 						width_shift_range=0.2,
 						height_shift_range=0.2,
-						horizontal_flip=True, 
-						validation_split=args.percent_val)
+						horizontal_flip=True)
 
-x_train = preprocess_input(x_train)
-x_val   = preprocess_input(x_val)
-
-
-
-# save checkpoints while training
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-												 save_weights_only=True,
-												 verbose=1)
-
-
-
-# create two datagen instances with the same arguments
-data_gen_args = dict()  # rotation_range=20,
-						# width_shift_range=0.2,
-						# height_shift_range=0.2,
-						# horizontal_flip=True, 
-						# validation_split=args.percent_val)
 
 image_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
 mask_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
